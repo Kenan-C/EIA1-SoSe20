04aeb839478968a1084eddb98494a86ad430b8b3
Kenan-C <64228072+Kenan-C@users.noreply.github.com>
2020-06-30 18:43:38 +0200

    Aufgabe 9 Update

--- Aufgabe_9/Todo.js
+++ Aufgabe_9/Todo.js
@@ -11,13 +11,19 @@ function Hinzufügen() {
     var neueAufgaben = document.querySelector("#Textfeld");
     aufgaben.push(neueAufgaben.value);
     document.querySelector("#Aufgaben").innerHTML += "<h3 id='Inhalt'>" + "&nbsp" + "&nbsp" + "<input type ='checkbox' id='klick'>" + aufgaben[i] + "&nbsp" + "&nbsp" + "<i class='fas fa-trash-alt' id='trash'> </i>";
-    document.querySelector("#trash").addEventListener("click", Löschen);
     document.querySelector("#Anzahl").innerHTML = "Anzahl Aufgaben: " + aufgaben.length;
     i++;
     console.log(aufgaben);
-    // Löschbutton funktioniert noch nicht richtig! Nur erste Aufgabe ist löschbar!
+}
+// Leider funktioniert mein Löschbutton immernoch nicht richtig. 
+// Mit Hilfe von Markus, problem mit Eventlistener am Löschen-Icon gelöst.
+// Habe es aber nicht hinbekommen, dass sich die einzelnen Aufgaben löschen lassen sondern nur komplett.
+var index;
+var trash = document.querySelectorAll("#trash");
+for (i = 0; i < aufgaben.length; i++) {
+    trash[i].addEventListener("click", Löschen);
     function Löschen() {
-        document.querySelector("#Inhalt").innerHTML = "";
+        document.querySelector("#Aufgaben").innerHTML = "";
         console.log("Löschen");
     }
 }
--- Aufgabe_9/Todo.ts
+++ Aufgabe_9/Todo.ts
@@ -18,18 +18,27 @@ function Hinzufügen (): void {
     var neueAufgaben: HTMLInputElement = document.querySelector("#Textfeld");
     aufgaben.push(neueAufgaben.value);
     document.querySelector("#Aufgaben").innerHTML += "<h3 id='Inhalt'>" + "&nbsp" + "&nbsp" + "<input type ='checkbox' id='klick'>" + aufgaben [i] + "&nbsp" + "&nbsp" + "<i class='fas fa-trash-alt' id='trash'> </i>";
-    document.querySelector("#trash").addEventListener("click", Löschen);
     document.querySelector("#Anzahl").innerHTML = "Anzahl Aufgaben: " + aufgaben.length;
     i++;
     console.log(aufgaben);
-
-// Löschbutton funktioniert noch nicht richtig! Nur erste Aufgabe ist löschbar!
-
-    function Löschen (): void {
-        document.querySelector("#Inhalt").innerHTML = "";
-        console.log("Löschen");
-    }
 }
+
+// Leider funktionieren die Löschbutton immernoch nicht richtig. 
+// Mit Hilfe von Markus, problem mit Eventlistener am Löschbutton gelöst.
+// Habe es aber nicht hinbekommen, dass sich die einzelnen Aufgaben löschen lassen sondern nur komplett.
+
+var index;
+var trash = document.querySelectorAll("#trash");
+
+for (i = 0; i < aufgaben.length ; i++) {
+        trash[i].addEventListener("click", Löschen);
+        function Löschen (): void {
+        document.querySelector("#Aufgaben").innerHTML = "";
+        console.log("Löschen");
+        }
+    }
+
+
 
 
 
